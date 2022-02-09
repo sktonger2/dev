de3a6d9cc0564c0341461aa3265fc30ecd052a91
sktonger <sktonger@users.noreply.github.com>
2022-02-09 17:44:02 +0530

    adding parameter to groovy

--- src/validate.groovy
+++ src/validate.groovy
@@ -1,5 +1,6 @@
 def validateTime(){
-echo 'validating time'
+    echo "validating time , start time: ${params.START_TIME} , end_time: ${params.END_TIME}"
+    // this is new comment
 }
 
 return this
