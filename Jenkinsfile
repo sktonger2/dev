c9aa07d80c91b0ba4c9b863f1111a02f8c2ad058
sktonger <sktonger@users.noreply.github.com>
2022-02-09 19:13:35 +0530

    added extra prameters for validation

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,8 +2,14 @@ def gv
 pipeline {
     agent any
     parameters{
-        string(name: 'START_TIME', defaultValue: '', description: 'Start time in epoch format')
-        string(name: 'END_TIME', defaultValue: '', description: 'End time in epoch format')
+        //yyyy-MM-dd HH:mm:ss	2012-01-31 23:59:59
+        string(name: 'START_DATE', defaultValue: '1970-01-31', description: 'Start date in YYYY-MM-DD format')
+        string(name: 'START_TIME', defaultValue: '23:59:59', description: 'Start time in HH:mm:ss format')
+
+        string(name: 'END_DATE', defaultValue: '1970-01-31', description: 'End date in YYYY-MM-DD format')
+        string(name: 'END_TIME', defaultValue: '23:59:59', description: 'End time in HH:mm:ss format')
+
+        
         choice(name: 'CLUSTER', choices: ['qe','prod'], description: 'deploy cluster details, it will be deployed in tlb-repair ns')
         
     }
--- src/validate.groovy
+++ src/validate.groovy
@@ -1,5 +1,39 @@
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+//String startdateStr = "05.08.2017 21:20:00"
+//String tzStr = "GMT"
+
+//gettime()
+
+//printTimestamp("05.08.2017 21:20:00", "GMT")
+//printTimestamp("06.08.2017 01:30:00", "GMT")
+
+// def printTimestamp(String dateStr, String tzStr){
+//    SimpleDateFormat fmt = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
+//    def tz = TimeZone.getTimeZone(tzStr)
+//    fmt.setTimeZone(tz)
+
+//    def date = fmt.parse(dateStr)
+//    println "$dateStr -> ${TimeUnit.MILLISECONDS.toSeconds(date.time)} secs"
+// }
+
+def Date getEpochTime(String dateStr,String timeStr){
+    //yyyy-MM-dd HH:mm:ss	2012-01-31 23:59:59
+    SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    def tz = TimeZone.getTimeZone("GMT")
+    fmt.setTimeZone(tz)
+    def timestamp = dateStr + " " + timeStr
+    def date = fmt.parse(timestamp)
+    return date
+    //return ${TimeUnit.MILLISECONDS.toSeconds(date.time)}
+
+
+}
 def validateTime(){
-    echo "validating time , start time: ${params.START_TIME} , end_time: ${params.END_TIME}"
+    def epochTime = getEpochTime( "${params.START_DATE}","${params.START_TIME}")
+    echo "start time is : ${TimeUnit.MILLISECONDS.toSeconds(epochTime.time)}"
+    //echo "validating time , start time: ${params.START_TIME} , end_time: ${params.END_TIME}"
     // this is new comment
 }
 
